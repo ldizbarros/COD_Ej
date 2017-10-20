2a527cc2ee8def11b31091e3543313be4535526c
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2017-10-20 09:50:17 +0200

    Commit Lau

--- src/cod/ej/CodEj.java
+++ src/cod/ej/CodEj.java
@@ -7,5 +7,7 @@ public class CodEj {
                 + "muelles que el retrete se ha tragao");
         System.out.println("Laura, miras demasiado los Simpson, esto no puede "
                 + "ser ehh, te voy a poner el control parental");
-    }   
+         System.out.println("sombra aqui y sombra alla maquillate maquillate "
+                 + "un espejo de cristal y mirate y mirate ");
+   }
 }
