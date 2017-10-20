fd02862b5b60e23f5b061e12989d8da7309a739e
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2017-10-20 08:45:04 +0200

    Inicio Proyecto

---
+++ src/cod/ej/CodEj.java
@@ -0,0 +1,10 @@
+package cod.ej;
+
+public class CodEj {
+
+   public static void main(String[] args) {
+        System.out.println("Cojo un muelle, lo tiro por el retrete ya son 235 "
+                + "muelles que el retrete se ha tragao");
+    }
+    
+}
