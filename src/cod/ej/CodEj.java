92d1cc91753afc86780e1b540a0f62bc6fd40f71
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2017-10-20 09:03:48 +0200

    Inicio Proyecto Commit 2

--- src/cod/ej/CodEj.java
+++ src/cod/ej/CodEj.java
@@ -5,11 +5,8 @@ public class CodEj {
    public static void main(String[] args) {
         System.out.println("Cojo un muelle, lo tiro por el retrete ya son 235 "
                 + "muelles que el retrete se ha tragao");
-        
-        
-        System.out.println("Laura, miras demasiado los Simpson, esto no puede ser ehh, te voy a poner el control parental");
-        
-        
+        System.out.println("Laura, miras demasiado los Simpson, esto no puede "
+                + "ser ehh, te voy a poner el control parental");
     }
     
 }
