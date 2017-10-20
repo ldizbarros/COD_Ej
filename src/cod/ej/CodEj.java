f42912ddd2e506ef10ed4fc1e001720d0ebc39dc
mbacelofernandez <mbacelofernandez@victoria12.danielcastelao.org>
2017-10-20 08:58:20 +0200

    Aportacion Miriam

--- src/cod/ej/CodEj.java
+++ src/cod/ej/CodEj.java
@@ -5,6 +5,11 @@ public class CodEj {
    public static void main(String[] args) {
         System.out.println("Cojo un muelle, lo tiro por el retrete ya son 235 "
                 + "muelles que el retrete se ha tragao");
+        
+        
+        System.out.println("Laura, miras demasiado los Simpson, esto no puede ser ehh, te voy a poner el control parental");
+        
+        
     }
     
 }
